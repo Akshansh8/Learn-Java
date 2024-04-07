80c15e136fc7c90bc4a50375c53e87f13637c309
Akshansh Sharma <akshanshsharma2001@gmail.com>
2024-04-08 00:37:39 +0530

    added new files

--- src/Day12InnerClasses/InnerClasses.java
+++ src/Day12InnerClasses/InnerClasses.java
@@ -1,6 +1,31 @@
 package Day12InnerClasses;
+class A{
+    public void show(){
+        System.out.println("Show of A ");
+    }
 
+    class B{
+        public void show(){
+            System.out.println("Show of B");
+        }
+    }
+
+    static class C{
+        public void show(){
+            System.out.println("Shoiw of C");
+        }
+    }
+}
 public class InnerClasses
 {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+        A.B obj1 = obj.new B();
+        obj1.show();
+        A.C obj2 = new A.C();
+
+
+    }
 
 }
